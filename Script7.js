403f404a403ece970a2882a7146cc2a770c6188d
msrankit <133192016+msrankit@users.noreply.github.com>
2026-02-02 14:00:15 +0530

    Add profile styling and highlight important elements

---
+++ Script7.js
@@ -0,0 +1,14 @@
+const profile=document.getElementById("profile");
+profile.style.backgroundColor="cyan";
+profile.style.padding="15px"
+profile.style.textAlign="center";
+//task-2
+const highlight = document.getElementsByClassName
+("important");
+console.log(highlight)
+
+for(let i=0;i<highlight.length;i++){
+    highlight[i].style.color="blue"
+    highlight[i].style.fontSize="30px"
+
+    }
